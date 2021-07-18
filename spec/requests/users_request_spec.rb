e97684d5b6e7c3dbe3a2699ec266308c75f87dbd
Shunyakatada <85655151+shunyasaitama@users.noreply.github.com>
2021-07-18 16:30:46 +0900

    テストの記述一部

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users", type: :request do
   describe "GET #index" do
     before do
+      get "/users"
       @user_1 = FactoryBot.create(:user)
       @user_2 = FactoryBot.create(:user, name: "テストユーザー2")
       @record_1 = FactoryBot.create(:record, user: @user_1)
@@ -16,7 +17,6 @@ RSpec.describe "Users", type: :request do
 
     context "ランキングの順番が合っているか" do
       it "@user_2が@user_1より上位であること" do
-        get '/users'
         expect(@user_2.overall_ranking).to be > @user_1.overall_ranking
       end
     end
